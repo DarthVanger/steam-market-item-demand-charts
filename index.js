b37a8a3519d698780db40c9197e08ca5af81cea8
DarthVanger <darthvanger@gmail.com>
2020-08-25 02:11:07 +0300

    Draw many graphs :)

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const url = `http://api.steamapis.com/market/item/${APP_ID}/${market_hash_name}?
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(handleGoogleChartLibLoad);
 
+const chartsContainer = document.querySelector('#charts')
+//chartsContainer.style.display = 'flex';
+//chartsContainer.style.flexWrap = 'wrap';
+
 function handleGoogleChartLibLoad() {
   console.log('google chart lib loaded');
 
@@ -25,48 +29,81 @@ function draw(itemData) {
   console.log('steam data fetched');
   console.log('itemData: ', itemData)
 
-  drawSellPriceChart(itemData)
-  drawQuantityChart({ title: 'Sell order graph', histogram: itemData.histogram.sell_order_graph })
-  drawQuantityChart({ title: 'Buy order graph', histogram:  itemData.histogram.buy_order_graph })
+  const itemInfoContainer = document.querySelector('#item-info')
+
+  itemInfoContainer.innerHTML = `<h1>${decodeURI(market_hash_name)}</h1>`
+
+  drawMedianAvgPrices(itemData.median_avg_prices_15days)
+  drawChart({
+    title: 'Sell order quantity',
+    isQuantityChart: true,
+    histogram: itemData.histogram.sell_order_graph
+  })
+  drawChart({
+    title: 'Sell order price',
+    isQuantityChart: false,
+    histogram:  itemData.histogram.sell_order_graph
+  })
+  drawChart({
+    title: 'Buy order quantity',
+    isQuantityChart: true,
+    histogram:  itemData.histogram.buy_order_graph
+  })
+  drawChart({
+    title: 'Buy order price',
+    isQuantityChart: false,
+    histogram:  itemData.histogram.buy_order_graph
+  })
 }
 
-function drawSellPriceChart(itemData) {
-  const sell_price_chart = itemData.histogram.sell_order_graph
+function drawMedianAvgPrices(avgPrices) {
+  console.log('avgPrices: ', avgPrices)
+
+  const chartData = avgPrices
     .map((item, index) => {
-      const [price, quantity, tooltip] = item;
+      const [day, price, quantity] = item;
       const uahRate = 27.47
-      const tooltipWithUah = tooltip.replace(/(\$[\S]+\s)/, `$1 (${Math.round(price * uahRate)} UAH) `);
-      return [index, price * uahRate, tooltipWithUah]
+      const priceUAH = Math.round(price * uahRate);
+      const tooltip = `Day: ${day}\nPrice (UAH): ${priceUAH}\nQuantity: ${quantity}`
+      return [day, priceUAH, tooltip]
     });
 
-  console.log('sell_price_chart: ', sell_price_chart)
+  console.log('chartData: ', chartData)
 
   var data = new google.visualization.DataTable();
 
-  data.addColumn('number', 'index');
+  data.addColumn('string', 'day');
   data.addColumn('number', 'price (UAH)');
   data.addColumn({type: 'string', role: 'tooltip'});
 
-  data.addRows(sell_price_chart);
+  data.addRows(chartData);
 
   var options = {
-    title: 'sell_price_chart',
+    title: 'Median average prices',
     curveType: 'function',
-    legend: { position: 'bottom' },
+    legend: 'none'
   };
 
-  var chart = new google.visualization.LineChart(document.getElementById('sell_price_chart'));
+  var chart = new google.visualization.LineChart(createChartElement());
 
   chart.draw(data, options);
 }
 
-function drawQuantityChart({ histogram, title }) {
+function drawChart({ histogram, title, isQuantityChart }) {
   console.log('histogram: ', histogram);
 
   const chart_data = histogram
     .map((item, index) => {
       const [price, quantity, tooltip] = item;
-      return [index, quantity, tooltip]
+
+      const uahRate = 27.47
+      const tooltipWithUah = tooltip.replace(/(\$[\S]+\s)/, `$1 (${Math.round(price * uahRate)} UAH) `);
+      const priceUAH = price * uahRate 
+
+      return isQuantityChart ? 
+        [index, quantity, tooltipWithUah]
+        :
+        [index, priceUAH, tooltipWithUah]
     });
 
   console.log('chart_data: ', chart_data)
@@ -74,7 +111,7 @@ function drawQuantityChart({ histogram, title }) {
   var data = new google.visualization.DataTable();
 
   data.addColumn('number', 'index');
-  data.addColumn('number', 'quantity');
+  data.addColumn('number', isQuantityChart ? 'quantity' : 'price');
   data.addColumn({type: 'string', role: 'tooltip'});
 
   data.addRows(chart_data);
@@ -82,15 +119,18 @@ function drawQuantityChart({ histogram, title }) {
   var options = {
     title,
     curveType: 'function',
-    legend: { position: 'bottom' }
+    legend: 'none'
   };
 
-  const element = document.createElement('div');
-  element.style.width = '900px';
-  element.style.height = '500px';
-  document.body.append(element);
-
-  var chart = new google.visualization.LineChart(element);
+  var chart = new google.visualization.LineChart(createChartElement());
 
   chart.draw(data, options);
+}
+
+function createChartElement() {
+  const element = document.createElement('div');
+  element.style.width = '100%';
+  //element.style.height = '700px';
+  chartsContainer.append(element);
+  return element;
 }
