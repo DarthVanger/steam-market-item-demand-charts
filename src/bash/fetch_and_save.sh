05a01c1c4c56b3d2a51840be019532493f2b1a86
EC2 Default User <ec2-user@ip-172-31-33-198.eu-west-3.compute.internal>
2020-08-28 22:17:42 +0000

    Correct fetch_and_save.sh to work from cron

--- src/bash/fetch_and_save.sh
+++ src/bash/fetch_and_save.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Fetching data from Steam API" >> log.txt
-node ../fetchAndSave.js >> log.txt 2>&1
+dirname="$(dirname "$0")"
+log="$dirname"/log.txt
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Fetching data from Steam API" >> "$log"
+/home/ec2-user/.nvm/versions/node/v14.9.0/bin/node "$dirname"/../fetchAndSave.js >> "$log" 2>&1
