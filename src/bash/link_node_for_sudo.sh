6c874e82d88a382accf4f1e15da89fa0d608ef22
DarthVanger <darthvanger@gmail.com>
2020-08-29 12:15:46 +0000

    Add a script to link node for sudo in AWS

---
+++ src/bash/link_node_for_sudo.sh
@@ -0,0 +1,4 @@
+node_path="/home/ec2-user/.nvm/versions/node/v14.9.0/bin"
+sudo ln -s "${node_path}"/node /usr/bin/node
+sudo ln -s "${node_path}"/npm /usr/bin/npm
+sudo ln -s "${node_path}"/npx /usr/bin/npx
