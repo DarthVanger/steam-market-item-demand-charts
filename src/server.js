c00569c611946646f4e39a1d096ee2247fdbc63d
DarthVanger <darthvanger@gmail.com>
2020-08-29 14:15:48 +0000

    Add endpoint to return index.html

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
 const mongo = require('./mongo');
 const steamApi = require('./steamApi');
 
@@ -11,22 +11,43 @@ async function run() {
 
 run().catch(console.dir);
 
+function respondWith500 (e) {
+  res.writeHead(500, {'Content-Type': 'text/plain'})
+  res.end(e.message);
+};
+
+async function respondWithData(res) {
+  const itemData = await getItemData();
+  console.log('Sending http response with data from Mongo');
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  res.write(Buffer.from(JSON.stringify(itemData)));
+  res.end();
+}
+
+async function responWithIndexHtml(res) {
+  try {
+    const file = await fs.readFile('./index.html', 'utf-8');
+    console.log('Successfully read index.html');
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(file);
+    res.end();
+  } catch (error) {
+    console.error('Failed to send index.html, error: ', error.message);
+    throw error;
+  }
+}
+
 async function startServer() {
   http.createServer((req, res) => {
-    console.log('Got http request');
-    respondWithData(res)
-      .catch ((e) => {
-        res.writeHead(500, {'Content-Type': 'text/plain'})
-        res.end(e.message);
-      });
+    const url = req.url;
+    console.log(`Got http request, url: ${url}`);
+    if (url === '/') {
+      responWithIndexHtml(res).catch(respondWith500);
+    }
 
-      async function respondWithData(res) {
-        const itemData = await getItemData();
-        console.log('Sending http response with data from Mongo');
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.write(Buffer.from(JSON.stringify(itemData)));
-        res.end();
-      }
+    if (url === '/item') {
+      respondWithData(res).catch (respondWith500);
+    }
   }).listen(80, () => {
     console.log('Server started, listening port 80');
   });
