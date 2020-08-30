d898fb10db5b5ffa37d20144399113b464cb731d
DarthVanger <darthvanger@gmail.com>
2020-08-30 09:43:51 +0000

    Run puppeteer with no sandbox to allow sudo

--- src/crawler.js
+++ src/crawler.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function fetchItemData(itemUrl) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+    });
     const page = await browser.newPage();
     let result;
 
