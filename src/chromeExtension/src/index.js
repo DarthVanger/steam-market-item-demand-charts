af0059865f7481df4872adf16c1f3b7741d97127
DarthVanger <darthvanger@gmail.com>
2020-09-04 19:51:51 +0000

    WIP: use webpack for chromExtension

--- src/chromeExtension/src/index.js
+++ src/chromeExtension/src/index.js
@@ -1,15 +1,20 @@
-function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', file);
-    th.appendChild(s);
-}
+import { createChart } from 'lightweight-charts';
 
-injectScript( chrome.extension.getURL('injectedScript.js'), 'body');
-
-//var port = chrome.runtime.connect();
-
+const chart = createChart(document.body, { width: 400, height: 300 });
+const lineSeries = chart.addLineSeries();
+lineSeries.setData([
+    { time: '2019-04-11', value: 80.01 },
+    { time: '2019-04-12', value: 96.63 },
+    { time: '2019-04-13', value: 76.64 },
+    { time: '2019-04-14', value: 81.89 },
+    { time: '2019-04-15', value: 74.43 },
+    { time: '2019-04-16', value: 80.01 },
+    { time: '2019-04-17', value: 96.63 },
+    { time: '2019-04-18', value: 76.64 },
+    { time: '2019-04-19', value: 81.89 },
+    { time: '2019-04-20', value: 74.43 },
+]);
+/*
 window.addEventListener("message", function(event) {
   // We only accept messages from ourselves
   if (event.source != window)
@@ -64,3 +69,4 @@ window.addEventListener("message", function(event) {
       });
   }
 }, false);
+*/
